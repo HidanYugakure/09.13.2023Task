1d40bae1532e3abebdaead987f447ae4596c995a
HidanYugakure <fidants@code.edu.az>
2023-12-08 22:28:16 +0400

    Add project files.

---
+++ ClassLibrary1/Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainBookStore.Models
+{
+    internal class Book : Product
+
+    {
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+
+        public Book(int ıD, string name, double price, int count, int totalCome, int sell) : base(ıD, name, price, count, totalCome, sell)
+        {
+
+        }
+
+        public override void Selll()
+        {
+            if (Count>0)
+            {
+                Count--;
+                TotalCome += Price;
+            }
+            else
+            {
+                Console.WriteLine("Kitablar satildi"); 
+            }
+            
+        }
+
+        public override void ShowInfo()
+        {
+            throw new NotImplementedException();
+        }
+  
+    }
+}
---
+++ ClassLibrary1/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DomainBookStore.Models
+{
+    public abstract class Product
+    {
+        private static int BookIdCounter = 1;
+        public int ID { get; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Count { get; set; }
+        public int TotalCome { get; set; }
+        public int Sell { get; set; }
+
+        public Product (int ıD, string name, int price, int count, int totalCome, int sell)
+        {
+            ID = BookIdCounter++;
+            Name = name;
+            Price = price;
+            Count = count;
+            TotalCome = totalCome;
+            Sell = sell;
+        }
+        public abstract void Selll();
+        public abstract void ShowInfo();
+    }
+    
+}
